92e76d1ed93e2b67032cd2e73b33ae4f869de337
Giovani Merces <t-giovani.silva@cni.com.br>
2024-11-22 11:29:51 -0300

    feat: create Checkbox component

---
+++ src/components/Checkbox/checkbox.css.ts
@@ -0,0 +1,96 @@
+import { theme } from '@/core/themes/default.css';
+import { style, styleVariants } from '@vanilla-extract/css';
+
+const checkboxWrapperBaseStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  gap: theme.spacing.xxxs,
+  padding: theme.spacing.xxxs,
+  cursor: 'pointer',
+  userSelect: 'none' as const,
+  transition: 'all 0.2s ease',
+  borderRadius: theme.borders.radius.sm,
+};
+
+export const checkboxWrapperVariantStyles = styleVariants({
+  light: {
+    color: theme.colors.neutrals.light[900],
+  },
+  dark: {
+    color: theme.colors.neutrals.dark[900],
+  },
+});
+
+export const checkboxWrapperStyle = style({
+  ...checkboxWrapperBaseStyle,
+  cursor: 'pointer',
+  ':hover': {
+    backgroundColor: theme.colors.primaryOverlay[100],
+  },
+});
+
+export const checkboxWrapperDisabledStyle = style({
+  ...checkboxWrapperBaseStyle,
+  cursor: 'not-allowed',
+});
+
+export const checkboxInputStyle = style({
+  appearance: 'none',
+  display: 'flex',
+  WebkitAppearance: 'none',
+  padding: theme.spacing['4xs'],
+  width: 16,
+  height: 16,
+  transition: 'all 0.2s ease',
+
+  borderRadius: theme.borders.radius.sm,
+  backgroundColor: theme.colors.neutrals.light[50],
+  borderStyle: 'solid',
+  borderWidth: theme.borders.width.thin,
+  borderColor: theme.colors.neutrals.light[200],
+  outline: 'none',
+  cursor: 'pointer',
+
+  selectors: {
+    '&:disabled': {
+      cursor: 'not-allowed',
+    },
+
+    //if checked then change the background color
+    '&:checked': {
+      backgroundColor: theme.colors.primary[400],
+      borderColor: theme.colors.primary[400],
+    },
+
+    [`${checkboxWrapperVariantStyles['dark']} &:disabled `]: {
+      backgroundColor: theme.colors.neutrals.dark[150],
+      borderColor: theme.colors.neutrals.dark[150],
+    },
+    [`${checkboxWrapperVariantStyles['dark']}:hover &:not(:disabled)`]: {
+      borderColor: theme.colors.primary[400],
+    },
+    [`${checkboxWrapperVariantStyles['light']} &:disabled`]: {
+      backgroundColor: theme.colors.neutrals.light[150],
+      borderColor: theme.colors.neutrals.light[150],
+    },
+    [`${checkboxWrapperVariantStyles['light']}:hover &:not(:disabled)`]: {
+      borderColor: theme.colors.primary[400],
+    },
+  },
+});
+
+export const checkboxLabelStyle = style({
+  fontSize: theme.fontSizes.sm,
+  fontWeight: theme.fontWeights.normal,
+  fontFamily: theme.fontFamily.default,
+  color: 'inherit',
+});
+
+export const checkboxIconStyle = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  pointerEvents: 'none',
+  position: 'absolute',
+  inset: 0,
+});
---
+++ src/components/Checkbox/index.tsx
@@ -0,0 +1,68 @@
+import { useColorScheme } from '@/providers';
+import clsx from 'clsx';
+import React from 'react';
+import {
+  checkboxInputStyle,
+  checkboxLabelStyle,
+  checkboxWrapperStyle,
+  checkboxWrapperVariantStyles,
+  checkboxIconStyle,
+  checkboxWrapperDisabledStyle,
+} from './checkbox.css';
+import { Icon } from '@/components/Icon';
+
+interface CheckboxProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+}
+
+export const Checkbox: React.FC<CheckboxProps> = ({
+  label,
+  checked,
+  disabled,
+  ...props
+}) => {
+  const { colorScheme } = useColorScheme();
+  return (
+    <label
+      onClick={(event: React.MouseEvent<HTMLLabelElement>) => {
+        if (disabled) {
+          event.preventDefault();
+          return;
+        }
+
+        if (props.onClick) {
+          const inputElement = event.currentTarget.querySelector('input');
+          if (inputElement) {
+            const inputEvent = new MouseEvent('click', {
+              bubbles: true,
+              cancelable: true,
+              view: window,
+            });
+            inputElement.dispatchEvent(inputEvent);
+          }
+        }
+      }}
+      className={clsx(
+        checkboxWrapperVariantStyles[colorScheme],
+        disabled ? checkboxWrapperDisabledStyle : checkboxWrapperStyle,
+      )}
+    >
+      <div style={{ position: 'relative' }}>
+        <input
+          type="checkbox"
+          className={clsx(checkboxInputStyle)}
+          checked={checked}
+          disabled={disabled}
+          {...props}
+          aria-checked={checked}
+        />
+        {checked && (
+          <span className={clsx(checkboxIconStyle)}>
+            <Icon name="check" color="white" size={16} />
+          </span>
+        )}
+      </div>
+      <span className={clsx(checkboxLabelStyle)}>{label}</span>
+    </label>
+  );
+};
