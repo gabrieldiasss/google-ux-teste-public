176d9a526f7cf4a4e71a0e2385dc0d87bde05ee6
Giovani Merces <t-giovani.silva@cni.com.br>
2024-11-23 14:01:22 -0300

    feat: add defaultColorScheme property to ColorSchemeProvider

--- src/providers/ColorSchemeProvider/index.tsx
+++ src/providers/ColorSchemeProvider/index.tsx
@@ -31,13 +31,20 @@ export const useColorScheme = (): ColorSchemeContextType => {
 
 interface ColorSchemeProviderProps {
   children: ReactNode;
+  defaultColorScheme?: ColorScheme;
 }
 
-export const ColorSchemeProvider = ({ children }: ColorSchemeProviderProps) => {
+export const ColorSchemeProvider = ({
+  children,
+  defaultColorScheme,
+}: ColorSchemeProviderProps) => {
   const getInitialColorScheme = (): ColorScheme => {
     const savedScheme = localColorScheme as ColorScheme;
     if (savedScheme) {
       return savedScheme;
+    }
+    if (defaultColorScheme) {
+      return defaultColorScheme;
     }
     return window.matchMedia('(prefers-color-scheme: dark)').matches
       ? ColorSchemeEnum.Dark
