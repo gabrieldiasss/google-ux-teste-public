12063ba5f401e146770282750c089d4ab9851a09
Giovani Merces <t-giovani.silva@cni.com.br>
2024-11-23 13:20:48 -0300

    fix: add color scheme support to Logo component

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import clsx from 'clsx';
 import { logoSizes } from './logo.css';
-import logo from './../../assets/logo.svg';
+import logoLight from './../../assets/logo-light.svg';
+import logoDark from './../../assets/logo-dark.svg';
+import { useColorScheme } from '@/providers';
 interface LogoProps {
   size?: 'xs' | 'md' | 'xl';
 }
 export const Logo: React.FC<LogoProps> = ({ size = 'md' }) => {
+  const { colorScheme } = useColorScheme();
+
   return (
     <img
-      src={logo}
+      src={colorScheme === 'light' ? logoLight : logoDark}
       alt="Logo do SENAI azul com o pingo do i em laranja"
       className={clsx([logoSizes[size]])}
     />
